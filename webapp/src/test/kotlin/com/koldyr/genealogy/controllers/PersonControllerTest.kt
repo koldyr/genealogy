b469968b2b36ddaf48ec20677376f630c1844faf
Denis Halitsky <koldyr@users.noreply.github.com>
2021-10-23 19:31:15 +0300

    Improved tests

--- webapp/src/test/kotlin/com/koldyr/genealogy/controllers/PersonControllerTest.kt
+++ webapp/src/test/kotlin/com/koldyr/genealogy/controllers/PersonControllerTest.kt
@@ -1,67 +1,54 @@
-package com.koldyr.genealogy.test.controllers
+package com.koldyr.genealogy.controllers
 
 import com.koldyr.genealogy.Genealogy
-import com.koldyr.genealogy.persistence.FamilyRepository
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType
 import org.springframework.test.annotation.IfProfileValue
 import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
+/**
+ * Description of class PersonControllerTest
+ * @created: 2021-10-23
+ */
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [Genealogy::class])
 @AutoConfigureMockMvc
 @TestPropertySource(properties = ["spring.config.location = classpath:application-test.yaml"])
 @IfProfileValue(name = "spring.profiles.active", values = ["int-test"])
-class FamilyControllerTest {
-
-    @Autowired
-    lateinit var familyRepository: FamilyRepository
+class PersonControllerTest {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
     @Test
-    fun families() {
-        mockMvc.get("/api/genealogy/families") {
-            accept = APPLICATION_JSON
+    fun persons() {
+        mockMvc.get("/api/genealogy/persons") {
+            accept = MediaType.APPLICATION_JSON
         }
             .andDo { print() }
             .andExpect {
                 status { isOk() }
-                content { contentType(APPLICATION_JSON) }
+                content { contentType(MediaType.APPLICATION_JSON) }
                 content { json("[]") }
             }
     }
 
     @Test
     fun events() {
-        mockMvc.get("/api/genealogy/families/1/events") {
-            accept = APPLICATION_JSON
+        mockMvc.get("/api/genealogy/persons/1/events") {
+            accept = MediaType.APPLICATION_JSON
         }
             .andDo { print() }
             .andExpect {
                 status { isOk() }
-                content { contentType(APPLICATION_JSON) }
-                content { json("[]") }
-            }
-    }
-
-    @Test
-    fun children() {
-        mockMvc.get("/api/genealogy/families/1/children") {
-            accept = APPLICATION_JSON
-        }
-            .andDo { print() }
-            .andExpect {
-                status { isOk() }
-                content { contentType(APPLICATION_JSON) }
+                content { contentType(MediaType.APPLICATION_JSON) }
                 content { json("[]") }
             }
     }
