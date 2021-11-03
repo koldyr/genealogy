0c5669b51387078582e8c5913488d99ceb544cd6
Yan Efimov <Yan_Efimov@swissre.com>
2021-11-03 15:24:45 +0200

    fix model Credentials

--- model/src/main/kotlin/com/koldyr/genealogy/model/Credentials.kt
+++ model/src/main/kotlin/com/koldyr/genealogy/model/Credentials.kt
@@ -1,10 +1,6 @@
 package com.koldyr.genealogy.model
 
-import org.jetbrains.annotations.NotNull
-
 class Credentials {
-    @NotNull
-    lateinit var username: String
-    @NotNull
-    lateinit var password: String
+    var username: String = ""
+    var password: String = ""
 }
